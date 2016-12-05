f2b0402640f6e05569a8105535c2415a1d986910
Nick Tomlin <nick.tomlin@gmail.com>
2016-12-05 17:54:20 -0600

    start tearing things down to troubleshoot basic connectivity

--- test/integration/bin.js
+++ test/integration/bin.js
@@ -12,39 +12,10 @@ function configPath (filename) {
 
 function spawnFlake (flakeArgs = []) {
   let proc = spawn('./bin/protractor-flake', flakeArgs)
-  proc.stdout.on('data', (buff) => {
-    process.stdout.write(`spawn stdout: ${buff.toString()}`)
-  })
-
-  proc.stderr.on('data', (buff) => {
-    process.stdout.write(`spawn stderr: ${buff.toString()}`)
-  })
-
   return proc
 }
 
 describe('Protractor Flake Executable', function () {
-  before((done) => {
-    console.log('PORT', process.env.PORT)
-    server.listen({port: process.env.PORT}, () => {
-      done()
-    })
-  })
-
-  after(() => {
-    server.close()
-  })
-
-  beforeEach((done) => {
-    unlink(FLAKE_FILE, function (err) {
-      if (err && err.code !== 'ENOENT') {
-        return done(err)
-      }
-
-      done()
-    })
-  })
-
   it('integration: Exits successfully if test passes before max limit is reached', (done) => {
     let proc = spawnFlake(['--max-attempts', '3', '--', configPath('sharded')])
     proc.on('close', (status) => {
@@ -52,26 +23,4 @@ describe('Protractor Flake Executable', function () {
       done()
     })
   })
-
-  // it('integration: exits unsuccessfully if test fails outside of max limit', (done) => {
-  //   let proc = spawnFlake(['--max-attempts', '1', '--', configPath('always-fail')])
-  //   proc.on('close', (status) => {
-  //     expect(status).to.equal(1)
-  //     done()
-  //   })
-  // })
-  //
-  // it('integration: exits with error if invalid parser is specified', (done) => {
-  //   let output = ''
-  //   let proc = spawnFlake(['--max-attempts', '3', '--parser', 'foo', '--', configPath('sharded')])
-  //   proc.stderr.on('data', (buff) => {
-  //     output += buff.toString()
-  //   })
-  //
-  //   proc.on('close', (status) => {
-  //     expect(status).to.equal(1)
-  //     expect(output).to.contain('Error: Invalid Parser Specified: foo')
-  //     done()
-  //   })
-  // })
 })
