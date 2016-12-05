35d20d31c7b228a71c0479ff841cd3e0faac786d
Nick Tomlin <nick.tomlin@gmail.com>
2016-12-04 21:35:27 -0600

    test

--- test/integration/support/flakey-test.js
+++ test/integration/support/flakey-test.js
@@ -4,6 +4,6 @@ describe('a flakey integration test', function () {
   it('fails, in a horribly consistent manner', function () {
     browser.get(`/flake/${TIMES_TO_FLAKE}`)
 
-    expect($('#success').isPresent()).toBeTruthy()
+    expect($('body').getText()).toEqual('Hello.')
   })
 })
