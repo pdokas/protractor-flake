455a83239e119f7a1ebf59b4e5c3e8d24b04850a
Nick Tomlin <nick.tomlin@gmail.com>
2016-12-07 08:08:52 -0600

    capabilities

--- test/integration/support/protractor-config/sharded.conf.js
+++ test/integration/support/protractor-config/sharded.conf.js
@@ -1,6 +1,7 @@
 'use strict';
 
-var JOB_NAME = 'Flake' + (process.env.TRAVIS_JOB_NUMBER || '');
+var JOB_NUMBER = (process.env.TRAVIS_JOB_NUMBER || '')
+var JOB_NAME = 'Flake' + JOB_NUMBER
 
 console.log('SAUCE_USERNAME', process.env.SAUCE_USERNAME)
 console.log('SAUCE KEY', process.env.SAUCE_ACCESS_KEY.slice(0, 3))
@@ -18,7 +19,8 @@ exports.config = {
     browserName: 'chrome',
     platform: 'OS X 10.9',
     version: 'latest',
-    name: JOB_NAME
+    name: JOB_NAME,
+    tunnel-identifier: JOB_NUMBER
   },
 
   baseUrl: 'http://127.0.0.1:3000',
